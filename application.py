0cfaa094188855b0e52cf81fb32a7606e7437a05
AELGyeom <ksg_0320@kaist.ac.kr>
2022-11-26 20:28:07 +0900

    DS4 ready 2

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ def fetchTask():
         results.append(r.PersonID)
 
     index = len(set(results)) % 6
-    possibleVersions=[2]
+    possibleVersions=[3]
     possibleTasks = [[100, 101], [100, 110], [101, 100], [101, 110], [110,100], [110, 101]]
     return [possibleVersions[index//6], possibleTasks[index%6]]
     # return random.sample(possibleTasks,1)[0]
