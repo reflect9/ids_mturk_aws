8a7415d53e8b18aaed4f6b4b6ea631ba3745e93d
AELGyeom <ksg_0320@kaist.ac.kr>
2022-12-20 22:11:42 +0900

    DS1 3conditions

--- application.py
+++ application.py
@@ -19,10 +19,10 @@ def fetchTask():
         results.append(r.PersonID)
 
     index = len(set(results)) % 6
-    possibleVersions=[0, 1, 2]
-    possibleTasks = [[100, 101], [101, 100]]
+    possibleVersions=[0]
+    possibleTasks = [[101, 110], [110, 101], [100, 110], [110, 100], [100, 101], [101, 100]]
     # possibleTasks = [[100, 101], [100, 110], [101, 100], [101, 110], [110,100], [110, 101]]
-    return [possibleVersions[index//2], possibleTasks[index%2]]
+    return [possibleVersions[index//6], possibleTasks[index%6]]
     # return random.sample(possibleTasks,1)[0]
 
 ################################################################
